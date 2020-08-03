e6f99355d2ef71874a8b159adb8778059a733a2c
Mauro Palgi <maurolink@gmail.com>
2020-08-03 18:27:03 -0300

    part1-c

--- part1/react/part1/src/index.js
+++ part1/react/part1/src/index.js
@@ -1,27 +1,108 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
-  return (
+//! lesson C - part 1
+
+// const Hello = ({age, name}) => {
+//   const bornYear = () => {
+//     const yearNow = new Date().getFullYear()
+//     return yearNow - age;
+//   }
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were Probably born in {bornYear()} </p>
+//     </div>
+//   )
+// }
+
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   )
+// }
+
+//ReactDOM.render(<App />, document.getElementById('root'))
+
+//! End lesson C - part 1
+
+//! lesson C - part 2
+
+// let counter = 1
+
+// const refresh = () => {
+//   ReactDOM.render(<App counter={counter} />, document.getElementById("root"))
+// }
+
+// setInterval( () => {
+//   refresh()
+//   counter += 1 
+// }, 1000)
+
+//! End lesson C - part 2
+
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Increment = ({handleClick, text}) => {
+  return(
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <button onClick={handleClick}>{text}</button>
     </div>
+
   )
 }
 
+const Decrement = ({handleClick, text}) => {
+  return(
+    <div>
+      <button onClick={handleClick}>{text}</button>
+    </div>
+
+  )
+}
+
+const Reset = ({handleClick, text}) => {
+  return(
+    <div>
+      <button onClick={handleClick}>{text}</button>
+    </div>
+  )
+}
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [counter, setCounter] = useState(0)
 
-  return (
+  // setTimeout(() => {
+  //  return setCounter(counter < 10 ? counter + 1: "Termino el show")  
+  // }, 1000) 
+
+  // const handleClick = () => {
+  //   console.log("Clicked", counter);
+  //   return setCounter(counter +1)
+  // }
+  const incrementByOne = () => setCounter(counter + 1);
+  const decrementByOne = () => setCounter(counter - 1);
+  const reset = () => setCounter(0);
+
+  return(
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+    <Display counter={counter} />
+    <Increment handleClick={incrementByOne} text="plus"/>
+    <Decrement handleClick={decrementByOne} text="minus"/>
+    <Reset handleClick={reset} text="reset"/>    
     </div>
-  )
-}
+    )
+  }    
+  
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"))
